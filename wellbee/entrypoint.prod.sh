cf1cc8610f24dcad7b5b7a4f9a6bdcba24623d28
Kohei Ruike <rui@KoheinoMacBook-Pro.local>
2026-04-27 22:41:00 +0900

    Add gunicorn access and error log output for debugging

--- wellbee/entrypoint.prod.sh
+++ wellbee/entrypoint.prod.sh
@@ -8,4 +8,7 @@ exec gunicorn wellbee.wsgi:application \
     --bind 0.0.0.0:8000 \
     --workers 2 \
     --threads 2 \
-    --timeout 60
+    --timeout 60 \
+    --access-logfile - \
+    --error-logfile - \
+    --log-level debug
